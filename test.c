c1d35ba813deee7bfc327082292031abc17952c4
Darian SOPORAN <dsoporan@e1p59.cluj.42.fr>
2017-06-08 15:59:23 +0300

    starting

---
+++ test.c
@@ -0,0 +1,154 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsoporan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/08 13:53:28 by dsoporan          #+#    #+#             */
+/*   Updated: 2017/06/08 15:43:51 by dsoporan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <math.h>
+#include <mlx.h>
+
+#define Width 512
+#define Height 384
+#define mapWidth 24
+#define mapHeight 24
+
+int g_worldMap[mapWidth][mapHeight] = 
+{
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+
+int main(/*int argc, char **argv*/)
+{
+	double posX = 22, posY = 12;
+	double dirX = -1, dirY = 0;
+	double planeX = 0, planeY = 0.66;
+
+	double time = 0;
+	double oldTime = 0;
+
+	void *mlx;
+
+	mlx = mlx_init();
+	mlx_new_window(mlx, Width, Height, "Raycaster");
+
+	int x = 0;
+	while (x < Width)
+	{
+		double cameraX = 2 * x / double(Width) - 1; //x-coordinate in camera space
+		double rayPosX = posX;
+		double rayPosY = posY;
+		double rayDirX = dirX + planeX * cameraX;
+		double rayDirY = dirY + planeY * cameraX;
+
+		int mapX = int(rayPosX);
+		int mapY = int(rayPosY);
+
+		double sideDistX;
+		double sideDistY;
+
+		double deltaDistX = sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX));
+		double deltaDistY = sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY));
+		double perpWallDist;
+
+		int stepX;
+		int stepY;
+
+		if (rayDirX < 0)
+		{
+			stepX = -1;
+			sideDistX = (rayPosX - mapX) * deltaDistX;
+		}
+		else
+		{
+			stepX = 1;
+			sideDistX = (mapX + 1.0 - rayPosX) * deltaDistX;
+		}
+		if (rayDirY < 0)
+		{
+			stepY = -1;
+			sideDistY = (rayPosY - mapY) * deltaDistY;
+		}
+		else
+		{
+			stepY = 1;
+			sideDistY = (mapY + 1.0 - rayPosY) * deltaDistY;
+		}
+
+		while (hit == 0)
+		{
+			//jump to next map square, OR in x-direction, OR in y-direction
+			if (sideDistX < sideDistY)
+			{
+				sideDistX += deltaDistX;
+				mapX += stepX;
+				side = 0;
+			}
+			else
+			{
+				sideDistY += deltaDistY;
+				mapY += stepY;
+				side = 1;
+			}
+			//Check if ray has hit a wall
+			if (worldMap[mapX][mapY] > 0) hit = 1;
+		}
+
+		if (side == 0) perpWallDist = (mapX - rayPosX + (1 - stepX) / 2) / rayDirX;
+		else           perpWallDist = (mapY - rayPosY + (1 - stepY) / 2) / rayDirY;
+
+		int lineHeight = (int)(h / perpWallDist);
+		int drawStart = -lineHeight / 2 + h / 2;
+		if (drawStart < 0)
+			drawStart = 0;
+		int drawEnd = lineHeight / 2 + h / 2;
+		if (drawEnd >= h)
+			drawEnd = h - 1;
+
+		ColorRGB color;
+		switch(worldMap[mapX][mapY])
+		{
+			case 1:  color = RGB_Red;  break; //red
+			case 2:  color = RGB_Green;  break; //green
+			case 3:  color = RGB_Blue;   break; //blue
+			case 4:  color = RGB_White;  break; //white
+			default: color = RGB_Yellow; break; //yellow
+		}
+
+		if (side == 1) 
+			color = color / 2;
+		//draw the pixels of the stripe as a vertical line mlx_img_addr
+		x++;
+	}
+
+	mlx_loop(mlx);
+	return (0);
+}
